52afd3c0efc8fff68866901be83edca67b080cb1
dominikkolesar <dominik.kolesar@akademiasovy.sk>
2016-03-30 14:22:17 +0200

    quiz v.1.0

---
+++ Otazky/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Otazky
+{
+    public partial class Form1 : Form
+    {
+        Color col;
+        Info info = new Info();
+        private int score;
+        public Form1()
+        {
+            InitializeComponent();
+            col = label1.BackColor;
+        }
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label7.Text = "";
+        }
+       
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isFirst();
+            isSecond();
+            isThird();
+            isFourth();
+            isFifth();
+            isSixth();
+            label7.Text = "your score: "+score.ToString();
+        }
+
+        public void isFirst()
+        {
+            if (info.firstQestion.Equals(textBox1.Text.ToUpper()))
+                score += 1;
+            else label1.BackColor = Color.Red;
+        }
+
+        public void isSecond()
+        {
+            if (radioButton1.Checked == true)
+            {
+                score += 1;
+            }
+            else label2.BackColor = Color.Red;
+        }
+        public void isThird()
+        {
+            if (checkBox1.Checked == true && checkBox2.Checked == true)
+            {
+                score += 2;
+            }
+            else if(checkBox1.Checked != true || checkBox2.Checked != true)
+            {
+                 label3.BackColor = Color.Red;
+            }
+           else 
+            {
+                if (checkBox1.Checked == true)
+                {
+                    score += 1;
+                }
+                else if (checkBox2.Checked == true)
+                {
+                    score += 1;
+                }
+
+                
+            }
+           /* if (checkBox2.Checked == true)
+            {
+                score += 1;
+            }*/
+           
+        }
+        public void isFourth()
+        {
+            if (radioButton6.Checked == true)
+            {
+                score += 1;
+            }
+            else label4.BackColor = Color.Red;
+        }
+        public void isFifth()
+        {
+            if (comboBox1.SelectedItem == "Ring")
+            {
+                score += 1;
+            }
+            else label5.BackColor = Color.Red;
+        }
+        public void isSixth()
+        {
+            if ("1970-01-01" == dateTimePicker1.Value.ToString("yyyy-MM-dd") )
+            {
+                score += 1;
+            }
+            else label6.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = col;
+            label2.BackColor = col;
+            label3.BackColor = col; ;
+            label4.BackColor = col;
+            label5.BackColor = col;
+            label6.BackColor = col;
+            label7.BackColor = col;
+            label7.Text = "";
+
+        }
+    }
+}
---
+++ Otazky/Info.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Otazky
+{
+    class Info
+    {
+        public String firstQestion { set; get; }
+
+        public String getfirstQestion()
+        {
+            return firstQestion;
+        }
+        public Info()
+        {
+            firstQestion = "ARPANET";
+        }
+        
+    }
+}
